1e37478e90fa8f1aa136add0a34cbbdfbb630436
chaeyeon-yang <myday0827@naver.com>
2021-07-25 23:54:08 +0900

    210725

---
+++ bronze/math1/fourth dot.py
@@ -0,0 +1,13 @@
+import sys
+x=[]
+y=[]
+for _ in range(3):
+    a,b=map(int,sys.stdin.readline().split())
+    x.append(a); y.append(b)
+
+for i in range(3):
+    if x.count(x[i])==1:
+        a=x[i]
+    if y.count(y[i])==1:
+        b=y[i]
+print(a,b)
