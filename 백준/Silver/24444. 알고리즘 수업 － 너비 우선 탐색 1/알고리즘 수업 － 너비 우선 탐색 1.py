4a1ad7d8a58312ca2dd35b1188c6db526dfd9cca
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2023-10-10 11:45:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 612 ms, Memory: 142256 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -1,32 +1,39 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-n,m,r = map(int,input().split())
-graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
-visited[r] = 1
-cnt = 1
-q = deque([r])
+# 정점의 수 N, 간선의 수 M, 시작 정점 R
+N, M, R = map(int, input().split())
 
-# 양방향 간선 생성
-for _ in range(m):
-    a,b = map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+# 그래프 초기화
+graph =[[]*(N+1) for _ in range(N+1)]
+# 방문체크
+visited = [0]*(N+1)
 
-# 오름차순 정렬
-for i in range(1,n+1):
-    graph[i].sort()
+# q 선언
+q = deque([R])
+# 방문순서 저장
+cnt=1
+# 시작 정점 방문처리
+visited[R] = cnt
 
-while q:
+# 그래프 연결
+for i in range(M):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+for i in range(1, N+1) :
+  graph[i].sort()
+
+def bfs():
+  global cnt
+  while q:
     now = q.popleft()
-    for i in graph[now]:
-        # 방문 여부 체크
-        if visited[i]:
-            continue
+    for nxt in graph[now]:
+      if not visited[nxt]:
         cnt+=1
-        visited[i] = cnt
-        q.append(i)
-        
-print(*visited[1:], sep="\n")
+        visited[nxt] = cnt
+        q.append(nxt)
+
+bfs()
+for i in range(1, N+1):
+  print(visited[i])
