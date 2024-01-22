724f26acd0602b2615a0cd0f7b7d7b88d0b55bec
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-01-22 09:52:15 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 308 ms, Memory: 49400 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,31 +1,27 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-# 노드의 개수 N
 N = int(input())
-# 그래프 초기화
 graph = [[] for _ in range(N+1)]
-# 방문처리 초기화
 visited = [0]*(N+1)
 
-# 그래프 연결
-for i in range(1, N):
-  a, b = map(int, input().split())
-  graph[a].append(b)
-  graph[b].append(a)
+for i in range(N-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 def bfs():
-  q = deque([1])
-  visited[1] = 1
-  while q:
-    now = q.popleft()
-    for nxt in graph[now]:
-      if not visited[nxt]:
-        visited[nxt] = now
-        q.append(nxt)
+    q = deque([1])
+    while q:
+        now = q.popleft()
+        for nxt in graph[now]:
+            if (visited[nxt] == 0):
+                visited[nxt] = now
+                q.append(nxt)
 
 bfs()
-res = visited[2:]
-for i in res:
-  print(i)
+
+for i in range(2, N+1):
+    print(visited[i])
