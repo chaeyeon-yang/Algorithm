baf75b48444416f92946385886fd6e3c6fe87b1c
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-01-26 15:26:14 +0900

    [Silver IV] Title: 로프, Time: 112 ms, Memory: 35364 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+rope = []
+
+for i in range(N):
+    rope.append(int(input()))
+rope.sort(reverse=True)
+
+for i in range(N):
+    rope[i] = rope[i]*(i+1)
+
+print(max(rope))
