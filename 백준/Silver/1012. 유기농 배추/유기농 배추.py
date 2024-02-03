f3209e19006abf19e840574717ccef99e5956893
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-02-03 20:14:07 +0900

    [Silver II] Title: 유기농 배추, Time: 52 ms, Memory: 31468 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,32 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-def check(x, y):
-    return 0<=x<M and 0<=y<N
+sys.setrecursionlimit(10**6)
 
 def dfs(x, y):
-    dx = [1,0,-1,0]
-    dy = [0,1,0,-1]
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if (check(nx, ny) and graph[ny][nx]==1):
-            graph[ny][nx]=2
+
+        if (0<=nx<n and 0<=ny<m and graph[nx][ny]==1):
+            graph[nx][ny] = -1
             dfs(nx, ny)
+    return 1
 
-T = int(input())
-for i in range(T):
-    M, N, K = map(int, input().split())
-    graph = [[0]*M for _ in range(N)]
+for i in range(int(input())):
+    m, n, k = map(int, input().split())
+    graph = [[0]*m for _ in range(n)]
 
-    for _ in range(K):
+    for _ in range(k):
         a, b = map(int, input().split())
         graph[b][a] = 1
-    cnt=0
-    for i in range(M):
-        for j in range(N):
-            if (graph[j][i]==1):
-                dfs(i, j)
-                cnt+=1
+
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                cnt += dfs(i, j)
 
     print(cnt)
-
