fbd72298787f4565a74707a27542310a9e451393
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-04-01 20:57:11 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+  a = input().rstrip()
+  s = []
+
+  if a == ".":
+    break
+
+  for i in a:
+    if i == '(' or i == '[':
+      s.append(i)
+    elif i == ']':
+      if len(s)!=0 and s[-1] == '[':
+        s.pop()
+      else:
+        s.append(']')
+        break
+    elif i == ')':
+      if len(s)!=0 and s[-1] == '(':
+        s.pop()
+      else:
+        s.append(')')
+        break
+      
+  if len(s) == 0 :
+    print('yes')
+  else :
+    print('no')
