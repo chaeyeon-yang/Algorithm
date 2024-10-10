102c98688f65e337dd27d777a1c87b21a2987d25
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-10-10 17:50:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,21 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int a[9];
+int arr[9], sum;
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  for (int i = 0; i < 9; i++) {
-    cin >> a[i];
-  }
-  sort(a, a + 9);
+  for (int i = 0; i < 9; i++)
+    cin >> arr[i];
+  sort(arr, arr + 9);
   do {
-    int sum = 0;
-    for (int i = 0; i < 7; i++) sum += a[i];
+    sum = 0;
+    for (int i = 0; i < 7; i++)
+      sum += arr[i];
     if (sum == 100)
       break;
-  } while (next_permutation(a, a + 9));
+  } while (next_permutation(arr, arr + 9));
   for (int i = 0; i < 7; i++)
-    cout << a[i] << "\n";
-  return 0;
+  {
+    cout << arr[i] << "\n";
+  }
 }
