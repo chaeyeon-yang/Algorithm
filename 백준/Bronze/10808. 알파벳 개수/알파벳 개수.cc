3594f9929114d63b0e714c76d417dd36b1f7a59c
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-10-10 18:35:49 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,17 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-string str;
-int cnt[26];
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  cin >> str;
-  for(char a: str) {
-    cnt[a - 'a']++;
+
+string s;
+int arr[29];
+int main()
+{
+  cin >> s;
+  for(char a:s) {
+    arr[a-'a'] += 1;
   }
+
   for (int i = 0; i < 26; i++)
-    cout << cnt[i] << " ";
-  return 0;
+  {
+    cout << arr[i] << " ";
+  }
 }
