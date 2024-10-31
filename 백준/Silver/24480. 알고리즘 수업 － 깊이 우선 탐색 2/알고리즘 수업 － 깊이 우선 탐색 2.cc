a626f3ecef92baca58f680929d1368155f2307b3
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-10-31 23:19:08 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 248 ms, Memory: 10800 KB -BaekjoonHub

--- 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -2,16 +2,19 @@
 using namespace std;
 
 int n, m, r, a, b;
-vector<int> adj[100001]; // 인접 리스트
-int visited[100001]; // 방문 순서 저장 배열
-int order = 1;
+int cnt = 1;
+vector<int> adj[100002];
+int visited[100002];
 
 void dfs(int r) {
-    visited[r] = order++; // 방문 순서를 기록
-    for (int v : adj[r]) {
-        if (visited[v] == 0) { // 아직 방문하지 않은 정점이라면
-            dfs(v);
-        }
+    visited[r] = cnt;
+    // cout << "정점 " << r << "를 탐색\n";
+    // cout << r << "\n";
+    for (int nxt: adj[r]) {
+        if (visited[nxt]) continue;
+        // cout << "정점 " << r << "에서 " << nxt << "를 방문!\n";
+        cnt++;
+        dfs(nxt);
     }
 }
 
@@ -20,18 +23,18 @@ int main() {
     for (int i = 0; i < m; i++) {
         cin >> a >> b;
         adj[a].push_back(b);
-        adj[b].push_back(a); 
+        adj[b].push_back(a);
     }
-  
-    // 인접 리스트 내림차순 정렬
+
     for (int i = 1; i <= n; i++) {
         sort(adj[i].rbegin(), adj[i].rend());
     }
-  
+
     dfs(r);
 
-    // 각 정점의 방문 순서 출력 (1부터 N까지 출력)
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)   {
         cout << visited[i] << "\n";
     }
+
+    return 0;
 }
