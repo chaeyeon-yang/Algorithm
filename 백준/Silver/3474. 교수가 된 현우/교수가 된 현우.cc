c7df5c9085907460c8c81f3ea7e75642020b17ac
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2024-11-08 22:51:00 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int t, a;
+int main () {
+	ios_base::sync_with_stdio(false);cin.tie(NULL); cout.tie(NULL);
+	cin >> t; 
+	for(int i = 0; i < t; i++){
+		cin >> a; 
+		int ret2 = 0, ret5 = 0;
+		for(int j = 2; j <= a; j *= 2){
+			ret2 += a / j;  
+		}
+		for(int j = 5; j <= a; j *= 5){
+			ret5 += a / j; 
+		}
+		cout << min(ret2, ret5) << "\n";
+	} 
+	return 0;
+}
