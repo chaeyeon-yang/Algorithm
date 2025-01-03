9192f9099f662729343c7a858090532f5d3ddf8e
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-01-03 17:13:59 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -1,46 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-// 연결요소 개수
-// 인접행렬, dfs
-int t, m, n, k, adj[52][52], visited[52][52], y, x;
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
+// 연결요소 개수 찾기, 인접리스트
+const int max_size = 54;
+int a, b;
+int t, m, n, k, adj[max_size][max_size], visited[max_size][max_size];
+const int dy[4] = {-1,0,1,0};
+const int dx[4] = {0, 1, 0, -1};
 
 void dfs(int y, int x) {
   visited[y][x] = 1;
-  for (int i = 0; i < 4; i++){
+  for (int i = 0; i < 4; i++)
+  {
     int ny = y + dy[i];
     int nx = x + dx[i];
-    if (ny<0 || ny >=n || nx<0 || nx >=m || adj[ny][nx]== 0) continue;
-    if (visited[ny][nx]) continue;
+    if (ny<0 || ny>=n || nx<0 || nx>=m || visited[ny][nx]) continue;
+    if (adj[ny][nx] == 0) continue;
     dfs(ny, nx);
   }
   return;
 }
-
 int main() {
   cin >> t;
   for (int i = 0; i < t; i++) {
-    cin >> m >> n >> k;
     int cnt = 0;
+    cin >> m >> n >> k;
     memset(adj, 0, sizeof(adj));
     memset(visited, 0, sizeof(visited));
-    for (int i = 0; i < k; i++)
+
+    for (int j = 0; j < k; j++)
     {
-      cin >> x >> y;
-      adj[y][x] = 1;
+      cin >> a >> b;
+      adj[b][a] = 1;
     }
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        if (adj[i][j] == 1 && !visited[i][j]) {
+    for (int t = 0; t < n; t++) {
+      for (int r = 0; r < m; r++) {
+        if (!visited[t][r] && adj[t][r] == 1) {
           cnt++;
-          dfs(i, j);
+          dfs(t, r);
         }
       }
     }
     cout << cnt << "\n";
   }
-  
-  return 0;
 }
