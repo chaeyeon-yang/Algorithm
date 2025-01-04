431a8a637d58d2cbf0dd77e525cba4df655b9508
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-01-04 21:27:28 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2432 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,38 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n, adj[104][104], visited[104][104];
+int res = 1;
+const int dy[4] = {-1, 0, 1, 0};
+const int dx[4] = {0, 1, 0, -1};
 
-// 연결요소 개수
-int n, adj[102][102], visited[102][102], a, res = 1;
-
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
-
+// dfs
 void dfs(int y, int x, int h) {
   visited[y][x] = 1;
   for (int i = 0; i < 4; i++) {
     int ny = y + dy[i];
     int nx = x + dx[i];
-    if (ny<0 || ny>=n || nx<0 || nx>=n) continue;
-    if (!visited[ny][nx] && adj[ny][nx] > h)
-      dfs(ny, nx, h);
+    if (ny<0 || ny>=n || nx<0 || nx>=n || visited[ny][nx]) continue;
+    if (adj[ny][nx] <= h) continue;
+    dfs(ny, nx, h);
   }
   return;
 }
-int main()
-{
+
+int main() {
   cin >> n;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       cin >> adj[i][j];
     }
   }
-  for (int h = 1; h < 101; h++) {
+  for (int i = 1; i < 101; i++)  {
     int cnt = 0;
     memset(visited, 0, sizeof(visited));
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        if (adj[i][j] > h && !visited[i][j]) {
-          dfs(i, j, h);
+    for (int a = 0; a < n; a++) {
+      for (int b = 0; b < n; b++) {
+        if(adj[a][b] > i && !visited[a][b]) {
+          dfs(a, b, i);
           cnt++;
         }
       }
@@ -40,5 +39,4 @@ int main()
     res = max(res, cnt);
   }
   cout << res << "\n";
-  return 0;
 }
