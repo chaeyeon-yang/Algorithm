87396311148051df46188b2ac83b4e80a0e0977b
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-01-04 22:58:40 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2452 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,44 +1,45 @@
-#include<bits/stdc++.h> 
-using namespace std;  
-#define y1 aaaa 
-int a[104][104], m, n, k, x1, x2, y1, y2, visited[104][104];
+#include<bits/stdc++.h>
+using namespace std;
+int m, n, k, adj[104][104], visited[104][104];
+int a, b, c, d, cnt;
+vector<int> res;
 const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
-vector<int> res; 
-int dfs(int y, int x){
-    visited[y][x] = 1; 
-    int res = 1; 
-    for(int i = 0; i < 4; i++){
-        int ny = y + dy[i]; 
-        int nx = x + dx[i]; 
-        if(ny < 0 || ny >= m || nx < 0 || nx >= n || visited[ny][nx] == 1) continue; 
-		if(a[ny][nx] == 1) continue;
-        res += dfs(ny, nx);
-    } 
-    return res; 
+const int dx[4] = {0,1,0,-1};
+
+int dfs(int y, int x) {
+  visited[y][x] = 1;
+  int size = 1;
+  for (int i = 0; i < 4; i++) {
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+    if (ny<0 || ny >= m || nx<0 || nx>=n || visited[ny][nx] || adj[ny][nx]) continue;
+    size += dfs(ny, nx);
+  }
+  return size;
 }
-int main() { 
-    ios_base::sync_with_stdio(false);  
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> m >> n >> k; 
-    for(int i = 0; i < k; i++){
-        cin >> x1 >> y1 >> x2 >> y2;  
-        for(int x = x1; x < x2; x++){
-            for(int y = y1; y < y2; y++){
-                a[y][x] = 1; 
-            }
-        }
+int main() {
+  cin >> m >> n >> k;
+  for (int i = 0; i < k; i++) {
+    cin >> a >> b >> c >> d;
+    for (int t = a; t < c; t++) {
+      for (int r = b; r < d; r++) {
+        adj[r][t] = 1;
+      }
     }
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n; j++){
-            if(a[i][j] != 1 && visited[i][j] == 0) { 
-                res.push_back(dfs(i, j));
-            }
-        }
+  }
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if(!visited[i][j] && adj[i][j] == 0) {
+        res.push_back(dfs(i, j));
+        cnt++;
+      }
     }
-    sort(res.begin(), res.end());
-    cout << res.size() << "\n";
-    for(int a : res) cout << a << " "; 
-    return 0; 
+  }
+  cout << cnt << "\n";
+  sort(res.begin(), res.end());
+  for (int i : res) {
+    cout << i << " ";
+  }
+  return 0;
 }
