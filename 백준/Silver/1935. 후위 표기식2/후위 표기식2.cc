cfc6505eed6855879cb1d61e57a9f2d951840c3d
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-03-12 13:03:15 +0900

    [Silver III] Title: 후위 표기식2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.cc
@@ -0,0 +1,42 @@
+#include<algorithm>
+#include<stack>
+#include<cstring> 
+#include<iostream>
+#include<cmath>
+#include<iomanip> 
+
+using namespace std;
+int n, num;
+string s;
+stack<double> st;
+int arr[26];
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cin >> n >> s;
+  
+  for (int i = 0; i < n; i++) {
+    cin >> num;
+    arr[i] = num;
+  }
+  for (char i:s) {
+    if ('A' <= i && i <= 'Z') {
+      st.push(arr[i-'A']);
+    } else {
+      if(!st.empty()) {
+        double tmp2 = st.top();
+        st.pop();
+        double tmp = st.top();
+        st.pop();
+        if (i == '+') st.push(tmp + tmp2);
+        else if (i == '-') st.push(tmp - tmp2);
+        else if (i == '*') st.push(tmp * tmp2);
+        else if (i == '/') st.push(tmp / tmp2);
+      }
+    }
+  }
+
+  cout << fixed << setprecision(2) << st.top() << "\n";
+  return 0;
+}
