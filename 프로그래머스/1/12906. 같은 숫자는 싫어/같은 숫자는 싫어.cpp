0ba3a9070736a20afc0cf4d4b3897fd95b5c9b71
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-03-29 01:05:50 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 119.76 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -8,8 +8,9 @@ vector<int> solution(vector<int> arr)
     vector<int> answer;
     answer.push_back(arr[0]);
     for(int i=1; i<arr.size(); i++) {
-        if (arr[i-1]!=arr[i]) answer.push_back(arr[i]);
+        if (arr[i] == arr[i-1]) continue;
+        answer.push_back(arr[i]);
     }
-    
+
     return answer;
 }
