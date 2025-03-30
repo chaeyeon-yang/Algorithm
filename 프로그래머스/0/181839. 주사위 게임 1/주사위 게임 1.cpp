11b403065692cd2b48b375460f3e8fd744801f27
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-03-30 23:18:31 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.03 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    if (a%2!=0 && b%2!=0) {
+        answer = a*a + b*b;
+    } else if (a%2==0 && b%2==0) answer = abs(a-b);
+    else if ((a%2==0 && b%2!=0) || (b%2==0 && a%2!=0)) {
+        answer = 2*(a+b);
+    }
+        
+    return answer;
+}
