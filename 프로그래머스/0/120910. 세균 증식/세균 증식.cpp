6e5516787f89be67abefa17f4f323f639a0b668b
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-04-30 23:53:31 +0900

    [level 0] Title: 세균 증식, Time: 0.01 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, int t) {
+    int answer = 1;
+    for(int i=0; i<t; i++) {
+        answer *= 2;
+    }
+    answer*=n;
+    return answer;
+}
