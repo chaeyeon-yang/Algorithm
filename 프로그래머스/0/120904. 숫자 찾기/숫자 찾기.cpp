a63361544706938456ba1dba1b79501da7c3a5f2
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-05-03 21:50:14 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = -1;
+    string s = to_string(num);
+    for(int i=0; i<s.size(); i++) {
+        if (s[i]-'0' == k) {
+            answer = i+1;
+            break;
+        }
+    }
+    return answer;
+}
