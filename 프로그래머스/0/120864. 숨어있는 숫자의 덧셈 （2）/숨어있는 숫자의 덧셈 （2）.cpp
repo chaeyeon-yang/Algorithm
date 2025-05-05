ca8183d41fd49f699aea3127959b063cc70bfa54
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-05-05 17:23:17 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cpp
@@ -6,8 +6,6 @@ using namespace std;
 
 int solution(string my_string) {
     int answer = 0;
-    // 26개. 65+26 = 91 97+26 = 123
-    bool flag = true;
     string tmp = "";
     for(int i=0; i<my_string.size(); i++) {
         if (my_string[i] >= 'a' && my_string[i] <= 'z' ||
@@ -15,7 +13,6 @@ int solution(string my_string) {
             if(tmp!="") {
                 answer += stoi(tmp);
                 tmp = "";
-                flag = false;
             }
         } else {
             tmp += my_string[i];
