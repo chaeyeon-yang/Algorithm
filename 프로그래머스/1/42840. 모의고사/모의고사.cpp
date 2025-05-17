67eef47b4228b4734114699af635a1a0be8d698e
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-05-17 16:21:19 +0900

    [level 1] Title: 모의고사, Time: 0.32 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int num1 = 0, num2 = 0, num3 = 0;
+    deque<int> met1 = {1, 2, 3, 4, 5};
+    deque<int> met2 = {2, 1, 2, 3, 2, 4, 2, 5};
+    deque<int> met3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    for(int i = 0; i < answers.size(); i++) {
+        if (answers[i] == met1[i % met1.size()]) num1++;
+        if (answers[i] == met2[i % met2.size()]) num2++;
+        if (answers[i] == met3[i % met3.size()]) num3++;
+    }
+    
+    vector<pair<int, int>> scores = {
+        {num1, 1},
+        {num2, 2},
+        {num3, 3}
+    };
+    
+    // 점수 기준 내림차순 정렬
+    sort(scores.rbegin(), scores.rend());
+    
+    int max_score = scores[0].first;
+    
+    // 가장 높은 점수를 받은 사람 모두 answer에 추가
+    for (auto& p : scores) {
+        if (p.first == max_score) {
+            answer.push_back(p.second);
+        }
+    }
+    
+    sort(answer.begin(), answer.end()); 
+    return answer;
+}
