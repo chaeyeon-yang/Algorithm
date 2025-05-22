7f74f757b4d3ae122e84d239b5b48abd776bef4c
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-05-22 23:53:18 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.72 ms, Memory: 5.26 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+    map<char, int> mp;
+    
+    for(int i=0; i<s.size(); i++) {
+        if (mp[s[i]] == 0) {
+            mp[s[i]] = i+1;
+            answer.push_back(-1);
+        } else {
+            answer.push_back(i+1-mp[s[i]]);
+            mp[s[i]] = i+1;
+        }
+    
+    }
+    return answer;
+}
