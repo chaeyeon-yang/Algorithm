a8d1e2a0b981f21f91d782366a07078ba24d9421
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-07-04 20:31:10 +0900

    [Silver V] Title: 귀찮음, Time: 48 ms, Memory: 7124 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.cc
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -2,27 +2,42 @@
 
 using namespace std;
 
-int n, ans;
+int n;
+long long ans;
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
   cin >> n;
   vector<int> v(n);
-  for (int i = 0; i < n; i++) {
+  vector<int> tmp;
+  for (int i = 0; i < n; i++)
     cin >> v[i];
-  }
 
   sort(v.begin(), v.end());
-
-  for (int i = 0; i < n; i++) {
-    int now = v[i];
-    int sum = 0;
-    for (int j = i + 1; j < n; j++) {
-      sum += v[j];
+  for (int i = n - 1; i > 0; i--) {
+    if (tmp.size()==0) {
+      tmp.push_back(v[i]);
     }
-    ans += now * sum;
+    else {
+      tmp.push_back(tmp[tmp.size() - 1] + v[i]);
+    }
   }
+
+  // for(int i: tmp) {
+  //   cout << i << " ";
+  // }
+  // cout << "\n";
+
+  int j = tmp.size()-1;
+  for (int i = 0; i < n-1; i++)
+  {
+    int now = v[i];
+    ans += now * tmp[j];
+    // cout << now << " : " << tmp[j] << " \n";
+    j--;
+  }
+
   cout << ans << endl;
 
   return 0;
