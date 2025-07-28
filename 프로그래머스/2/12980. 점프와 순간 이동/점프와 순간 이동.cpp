7f0bdaecc9d5f1abfdf9dc9db61254c792c27a54
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-07-28 11:10:38 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-
 using namespace std;
 
 int solution(int n)
 {
-    int ans = 0;
-    while(n>0) {
-        if (n%2==0) {
-            n /= 2;
-        } else {
-            n-=1;
+    int ans = 1;
+    while(n!=1) {
+        if (n%2!=0) {
             ans++;
+            n-=1;
+        } else {
+            n/=2;
         }
     }
-    
+
     return ans;
 }
