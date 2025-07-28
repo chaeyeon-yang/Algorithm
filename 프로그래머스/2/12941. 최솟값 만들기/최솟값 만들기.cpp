a9ab41320aa414e5c290f60cebf7926a9e3b2bd0
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-07-28 11:04:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.09 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <vector>
+#include<vector>
 #include <algorithm>
-
 using namespace std;
 
 int solution(vector<int> A, vector<int> B)
@@ -10,7 +9,7 @@ int solution(vector<int> A, vector<int> B)
     sort(A.begin(), A.end());
     sort(B.rbegin(), B.rend());
     for(int i=0; i<A.size(); i++) {
-        answer+=A[i]*B[i];
+        answer += A[i]*B[i];
     }
 
     return answer;
