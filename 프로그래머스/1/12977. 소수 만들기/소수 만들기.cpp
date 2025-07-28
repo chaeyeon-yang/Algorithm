c49f1c60edfd724edd54581a931934815f665a1f
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-07-28 13:05:31 +0900

    [level 1] Title: 소수 만들기, Time: 0.65 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -1,25 +1,24 @@
 #include <vector>
 #include <iostream>
-#include <algorithm>
-
 using namespace std;
+
+bool sosu(int a, int b, int c) {
+    int tmp = a+b+c;
+    for(int i=2; i*i<=tmp; i++) {
+        if (tmp%i==0) return false;
+    }
+    return true;
+}
 
 int solution(vector<int> nums) {
     int answer = 0;
-    sort(nums.begin(), nums.end());
-    
     for(int i=0; i<nums.size(); i++) {
         for(int j=i+1; j<nums.size(); j++) {
             for(int k=j+1; k<nums.size(); k++) {
-                int tmp = nums[i]+nums[j]+nums[k];
-                bool flag = true;
-                for(int b=2; b<tmp; b++) {
-                    if (tmp%b==0) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) answer++;
+                bool isSosu = sosu(nums[i], nums[j], nums[k]);
+                
+                if (isSosu)
+                    answer++;
             }
         }
     }
