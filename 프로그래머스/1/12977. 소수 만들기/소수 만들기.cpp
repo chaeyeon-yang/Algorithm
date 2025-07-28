7165dcff0ca0bbd2e658f056bf951834c572c6c9
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-07-28 13:29:50 +0900

    [level 1] Title: 소수 만들기, Time: 0.66 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -2,27 +2,29 @@
 #include <iostream>
 using namespace std;
 
-bool sosu(int a, int b, int c) {
-    int tmp = a+b+c;
-    for(int i=2; i*i<=tmp; i++) {
-        if (tmp%i==0) return false;
+int answer = 0;
+
+bool isPrime(int num) {
+    if (num < 2) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
     }
     return true;
 }
 
-int solution(vector<int> nums) {
-    int answer = 0;
-    for(int i=0; i<nums.size(); i++) {
-        for(int j=i+1; j<nums.size(); j++) {
-            for(int k=j+1; k<nums.size(); k++) {
-                bool isSosu = sosu(nums[i], nums[j], nums[k]);
-                
-                if (isSosu)
-                    answer++;
-            }
-        }
+void backtrack(vector<int>& nums, int start, int count, int sum) {
+    if (count == 3) {
+        if (isPrime(sum)) answer++;
+        return;
     }
     
+    for (int i = start; i < nums.size(); i++) {
+        backtrack(nums, i + 1, count + 1, sum + nums[i]);
+    }
+}
 
+int solution(vector<int> nums) {
+    answer = 0;
+    backtrack(nums, 0, 0, 0);
     return answer;
 }
