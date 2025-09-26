d443563bb91625df1ac677be8561ed20feeb201d
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 19:26:43 +0900

    [level 2] Title: 전화번호 목록, Time: 94.80 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 bool solution(vector<string> phone_book) {
+    bool answer = true;
     sort(phone_book.begin(), phone_book.end());
-    for (int i = 0; i < phone_book.size() - 1; i++) {
-        string cur = phone_book[i];
-        string next = phone_book[i + 1];
-        if (next.substr(0, cur.size()) == cur) {
-            return false;
+    for(int i=0; i<phone_book.size()-1; i++) {
+        string pre = phone_book[i];
+        string en = phone_book[i+1];
+        if (en.find(pre) == 0) {
+            answer = false;
+            break;
         }
     }
-    return true;
+    
+    return answer;
 }
