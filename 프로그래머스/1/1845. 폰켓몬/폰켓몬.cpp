ec7bcaf7af810cc05063808c0f92ebeb656f45ea
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 18:55:01 +0900

    [level 1] Title: 폰켓몬, Time: 1.18 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -1,17 +1,24 @@
 #include <vector>
 #include <map>
+
 using namespace std;
 
 int solution(vector<int> nums) {
+    // 1: 1, 2: 1, 3: 2
+    // 2: 2, 3: 3, 4: 1
+    // 2: 3, 3: 3
     int answer = 0;
+    int tmp = 0;
     map<int, int> mp;
-    for(int n: nums) {
-        mp[n]++;
+    for(int i: nums) {
+        mp[i]++;
     }
-    for(auto a: mp) {
-        if (a.second > 0) answer++;
+    for(auto& a: mp) {
+        if (a.second >= 1) tmp++;
     }
-    if (answer > nums.size()/2) answer = nums.size()/2;
-
+    if (tmp >= nums.size()/2) answer = nums.size()/2;
+    else answer = tmp;
+    
+    
     return answer;
 }
