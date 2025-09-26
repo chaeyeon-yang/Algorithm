24cdee4fc3f0544bf5af841b2f0b65c7031b61c8
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 18:40:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 121.12 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,16 +7,17 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     map<string, int> mp;
-    for(string p: participant) {
-        mp[p]++;
+    for(string s: participant) {
+        mp[s]++;
     }
-    for(string c: completion) {
-        mp[c]--;
+    for(string s:completion) {
+        mp[s]--;
     }
-    
-    for(auto a: mp) {
-        if (a.second > 0) answer = a.first;
+    for(auto&a : mp) {
+        if (a.second >=1) {
+            answer = a.first;
+            break;
+        }
     }
-    
     return answer;
 }
