e41f9fa083fe934b658b3a1f2227c7faf4cbf2c7
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 22:18:43 +0900

    [level 2] Title: 올바른 괄호, Time: 0.53 ms, Memory: 3.88 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,29 +1,30 @@
-#include <string>
+#include<string>
 #include <iostream>
 #include <stack>
 
 using namespace std;
 
-bool solution(string s) {
-    stack<char> st;
-    
+bool solution(string s)
+{
     bool answer = true;
-    for(char c: s) {
-        if (c=='(') {
-            if (!st.empty() && st.top()==')') st.pop();
-            else st.push(c);
-        } else {
-            if (st.empty()) {
-                st.push(c);
-                break;
-            }
+    stack<char> st;
+    for(char c : s) {
+        if (st.empty()) {
+            if (c=='(') st.push(c);
             else {
-                if (!st.empty() && st.top()=='(') st.pop();
+                return false;
+            }
+        } else {
+            if (c=='(') {
+                if (st.top() == ')') st.pop();
+                else st.push(c);
+            } else {
+                if (st.top() == '(') st.pop();
                 else st.push(c);
             }
         }
     }
-    if(!st.empty()) answer = false;
+    answer = st.empty() ? true : false;
 
     return answer;
 }
