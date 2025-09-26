17815b02585b00c3c5075917c85d4ec50cd4879e
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 22:39:38 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,23 +1,39 @@
 #include <string>
-#include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 
-struct cmp {
-    bool operator()(pair<int, int> &a, pair<int, int> &b) {
-        if (a.first == b.first) 
-            return a.second > b.second;
-        return a.first > b.first;
-    }
-};
-
 int solution(vector<int> priorities, int location) {
+    // 2 1 3 2 => 1 3 2 2 => 3 2 2 1 => 3 
+    // 2 2 1
     int answer = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq;
-    for(int i=0; i<priorities.size(); i++) {
-        pq.push({priorities[i], i});
+    priority_queue<int> pq;
+    queue<pair<int, int>> q;
+    for(int i: priorities) {
+        pq.push(i);
     }
+    for(int i=0; i<priorities.size(); i++) {
+        q.push({i, priorities[i]});
+    }
+
+    int cnt = 0;
+    while(!q.empty()) {
+        int cur_index = q.front().first;
+        int cur_val = q.front().second;
+        int ma = pq.top();
+        if (cur_val < ma) {
+            q.pop();
+            q.push({cur_index, cur_val});
+        }
+        if (cur_val == ma) {
+            cnt++;
+            if (cur_index == location) {
+                return cnt;
+            }
+            pq.pop();
+            q.pop();
+        }
+    }
+   
     return answer;
 }
