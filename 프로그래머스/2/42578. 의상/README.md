aae32320ec171cb5c6d188fd8f0b2078bb14e024
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-09-26 19:49:04 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.02 ms
+메모리: 4.21 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 29일 01:02:21
+2025년 09월 26일 19:49:03
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -7,12 +7,14 @@ using namespace std;
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     map<string, int> mp;
-    for (int i=0; i<clothes.size(); i++) {
-        mp[clothes[i][1]]++;
+    for(int i=0; i<clothes.size(); i++) {
+        vector<string> tmp = clothes[i];
+        mp[tmp[1]]++;
     }
-    for(auto a: mp) {
-        answer *= a.second+1;
+    for(auto& a: mp) {
+        answer *= (a.second+1);
     }
     answer -= 1;
+    
     return answer;
 }
