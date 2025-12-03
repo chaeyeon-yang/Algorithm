3e9060eb99215ecdd09bc525c1d9834682aff078
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-12-03 23:42:10 +0900

    [level 2] Title: 지게차와 크레인, Time: 5.83 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.cpp
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.cpp
@@ -1,116 +1,87 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <cstring>
+ 
 using namespace std;
-
-int dy[4] = {-1,0,1,0};
-int dx[4] = {0,1,0,-1};
-
-int N, M;
+ 
 bool visited[51][51];
-
-// (sy, sx)에 있는 컨테이너가 외부와 연결되어 있는지 확인
-bool reachableToOutside(int sy, int sx, vector<string>& storage) {
-    // sy,sx 위치에는 target 컨테이너가 있고,
-    // 그 주변의 빈 칸들을 통해 바깥(테두리)에 도달 가능한지 확인
-    memset(visited, false, sizeof(visited));
-    queue<pair<int,int>> q;
-
-    // 컨테이너 주변의 빈칸들을 시작점으로 BFS
-    for (int d = 0; d < 4; d++) {
-        int ny = sy + dy[d];
-        int nx = sx + dx[d];
-        if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-        if (storage[ny][nx] == '.' && !visited[ny][nx]) {
-            visited[ny][nx] = true;
-            q.push({ny, nx});
-        }
-    }
-
-    while (!q.empty()) {
-        auto [y, x] = q.front(); q.pop();
-
-        // 이 빈 칸이 테두리에 닿아 있으면 => 외부와 연결
-        if (y == 0 || x == 0 || y == N-1 || x == M-1) {
-            return true;
-        }
-
-        for (int d = 0; d < 4; d++) {
-            int ny = y + dy[d];
-            int nx = x + dx[d];
-            if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-            if (visited[ny][nx]) continue;
-
-            // 빈 칸(이미 꺼낸 자리)만 타고 이동 가능
-            if (storage[ny][nx] == '.') {
-                visited[ny][nx] = true;
-                q.push({ny, nx});
-            }
-        }
-    }
-
-    return false; // 바깥으로 못 나감
-}
-
-// 지게차: 외부와 연결된 target 컨테이너만 제거
-void useForklift(char target, vector<string>& storage) {
-    vector<pair<int,int>> toRemove;
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (storage[i][j] != target) continue;
-
-            // 바로 테두리에 붙어 있다면 이미 외부와 한 면이 연결
-            if (i == 0 || j == 0 || i == N-1 || j == M-1) {
-                toRemove.push_back({i, j});
-            } else {
-                // 내부에 있는 경우: 주변 빈 칸들 통해 바깥까지 갈 수 있는지 BFS
-                if (reachableToOutside(i, j, storage)) {
-                    toRemove.push_back({i, j});
+ 
+void gCar(char target, vector<string> &storage, int n, int m){
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
+    
+    vector<pair<int, int>> delPoint;
+    
+    for(int idx=0; idx<n; idx++)
+        fill(visited[idx], visited[idx]+m, false);
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(i!=0 && i!=n-1 && j!=0 && j!=m-1) continue;
+            
+            if(storage[i][j]==target) delPoint.push_back(make_pair(i,j));
+            else if(storage[i][j] == ' '){
+                queue<pair<int, int>> q;
+                q.push(make_pair(i, j));
+                visited[i][j] = true;
+ 
+                while(!q.empty()){
+                    pair<int, int> xy = q.front();
+                    q.pop();
+                    
+                    int x = xy.first, y = xy.second;
+ 
+                    for(int dir=0; dir<4; dir++){
+                        int nx = x+dx[dir], ny = y+dy[dir];
+                        if(nx<0 || nx>=n || ny<0 || ny>=m) continue;
+ 
+                        if(storage[nx][ny]==target && !visited[nx][ny]) {
+                            delPoint.push_back(make_pair(nx,ny));
+                            visited[nx][ny] = true;
+                        }
+                        if(storage[nx][ny]==' ' && !visited[nx][ny])
+                        {
+                            q.push(make_pair(nx, ny));
+                            visited[nx][ny] = true;
+                        }
+                    }
                 }
             }
         }
     }
-
-    // 한 번에 제거 (중간에 바로 지우면 판정이 꼬임)
-    for (auto [y, x] : toRemove) {
-        storage[y][x] = '.';
-    }
+    for(pair<int, int> point : delPoint)
+        storage[point.first][point.second] = ' ';
 }
-
-// 크레인: 해당 종류 컨테이너 전체 제거
-void useCrane(char target, vector<string>& storage) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (storage[i][j] == target) {
-                storage[i][j] = '.';
-            }
+ 
+void crain(char target, vector<string> &storage, int n, int m){
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(storage[i][j] == target) storage[i][j] = ' ';
         }
     }
 }
-
+ 
 int solution(vector<string> storage, vector<string> requests) {
-    N = storage.size();
-    M = storage[0].size();
-
-    for (string req : requests) {
-        char item = req[0];
-
-        if (req.size() == 1) {
-            // 지게차
-            useForklift(item, storage);
-        } else {
-            // 크레인
-            useCrane(item, storage);
-        }
+    int answer = 0;
+    
+    int n = storage.size();
+    int m = storage[0].size();
+    
+    for(string cmd : requests)
+    {
+        if(cmd.size()==1)
+            gCar(cmd[0], storage, n, m);
+        else crain(cmd[0], storage, n, m);
     }
-
-    int cnt = 0;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (storage[i][j] != '.') cnt++;
-        }
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<m; j++)
+            if(storage[i][j] != ' ') answer++;
     }
-    return cnt;
+    return answer;
 }
