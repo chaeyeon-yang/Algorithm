732baac1d57f6b4db923af6d3dcbda63afce5cf5
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-12-03 22:11:32 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -1,30 +1,27 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <iostream>
 
 using namespace std;
-// 같은 시간대에 m명 늘어날때마다 서버 1대 추가 증설, 그 미만은 증설하지 않음
-// n x m명 이상 (n + 1) x m 미만 최소 n 대의 서버가 운영중이어야 함
-// 한 번 증설한 서버는 k시간 동안 운영하고 반납
-// k=5, 10~15시만 운영
-
-// 서버를 최소 몇번 증설? 
-map<int, int> mp;
 
 int solution(vector<int> players, int m, int k) {
     int answer = 0;
-    int arr[24];
-    int new_ser = 0;
+    // 게임을 이용하는 사람 m명 늘어날때마다 서버 +1
+    // nxm <= x < (n+1)xm --> 최소 n대의 증설된 서버가 운영중
+    // 한번 증설한 서버는 k시간 동안 운영
+    // 모든 게임 이용자가 게임하기위해 서버 최소 몇 번 증설
+    
+    vector<int> tmp(24); // 서버 운영상황
     for(int i=0; i<players.size(); i++) {
-        if (players[i]/m > 0) {
-            int tmp = players[i]/m-mp[i];
-            if (tmp <= 0) continue;
-            for(int j=i; j<i+k; j++) {
-                mp[j]+=tmp;
+        if(players[i] >= m) {
+            int need = players[i]/m - tmp[i];
+                      
+            if (need > 0) {
+                answer += need;
+                for(int j=i; j<i+k and j<24; j++) {
+                    tmp[j] += need;
+                }
             }
-
-            answer+=tmp;
         }
     }
     
