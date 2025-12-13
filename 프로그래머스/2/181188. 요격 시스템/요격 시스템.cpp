473d7a5ce49429af87049adcbd49db8b10734ce3
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-12-13 17:03:44 +0900

    [level 2] Title: 요격 시스템, Time: 153.72 ms, Memory: 96.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(vector<int> &v1, vector<int> &v2) {
+	if (v1[1] == v2[1])
+		return v1[0] < v2[0];
+	else return v1[1] < v2[1];
+}
+
+int solution(vector<vector<int>> targets) {
+	int answer = 0;
+	sort(targets.begin(), targets.end(), cmp);
+
+	int e = targets[0][1];
+	answer++;
+
+	for (int i = 1; i < targets.size(); i++) {
+		if (targets[i][0] < e) {
+			continue;
+		}
+		else {
+			answer++;
+			e = targets[i][1];
+        }
+	}
+	return answer;
+}
