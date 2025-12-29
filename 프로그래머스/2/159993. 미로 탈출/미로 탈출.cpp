338935dd02acfb8c0be0a981754a532aeac5efc1
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2025-12-29 23:49:12 +0900

    [level 2] Title: 미로 탈출, Time: 0.36 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -5,53 +5,67 @@
 
 using namespace std;
 
-const int dy[4] = {-1, 0, 1, 0};
-const int dx[4] = {0, 1, 0, -1};
+const int dy[4] = {-1,0,1,0};
+const int dx[4] = {0,1,0,-1};
 
-int n, m;
-
-int bfs(vector<string>& maps, pair<int,int> start, char target) {
-    vector<vector<int>> visited(n, vector<int>(m, 0));
-    queue<pair<int,int>> q;
-    q.push(start);
-    visited[start.first][start.second] = 1;
-
-    while (!q.empty()) {
+int bfs(pair<int,int> start, pair<int,int> end,
+        vector<string>& maps) {
+    int n = maps.size();
+    int m = maps[0].size();
+    queue<pair<int, int>> q;
+    vector<vector<int>> visited(n, vector<int>(m, -1));
+    q.push({start.first, start.second});
+    visited[start.first][start.second] = 0;
+    while(!q.empty()) {
         auto [y, x] = q.front();
         q.pop();
-
-        if (maps[y][x] == target) return visited[y][x] - 1;
-
-        for (int i = 0; i < 4; i++) {
+        if (y == end.first && x == end.second) {
+            return visited[y][x];
+        }
+        for(int i=0; i<4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+            if (ny<0 || nx<0 || ny>=n || nx >=m) continue;
             if (maps[ny][nx] == 'X') continue;
-            if (visited[ny][nx]) continue;
+            if (visited[ny][nx]!=-1) continue;
             visited[ny][nx] = visited[y][x] + 1;
-            q.push({ny, nx});
+            q.push(make_pair(ny, nx));
         }
     }
-    return -1; // 못 찾음
+    return -1;
 }
 
 int solution(vector<string> maps) {
-    n = maps.size();
-    m = maps[0].size();
-    pair<int,int> S, L, E;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (maps[i][j] == 'S') S = {i, j};
-            if (maps[i][j] == 'L') L = {i, j};
-            if (maps[i][j] == 'E') E = {i, j};
+    int answer = 0;
+    pair<int, int> st;
+    pair<int, int> ex;
+    pair<int, int> le;
+    
+    int n = maps.size();
+    int m = maps[0].size();
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if (maps[i][j] == 'S') {
+                st = {i, j};
+            } 
+            if (maps[i][j] == 'E') {
+                ex = {i, j};
+            }
+            if (maps[i][j] == 'L') {
+                le = {i, j};
+            }
         }
     }
-
-    int dist1 = bfs(maps, S, 'L');
-    if (dist1 == -1) return -1;
-    int dist2 = bfs(maps, L, 'E');
-    if (dist2 == -1) return -1;
-
-    return dist1 + dist2;
+    
+    // st 부터 le까지 bfs로 cnt 더하고
+    // le 부터 ex bfs로 cnt 더하기
+    
+    int st_le = bfs(st, le, maps);
+    int le_ex = bfs(le, ex, maps);
+    
+    if (st_le == -1 || le_ex == -1) return -1;
+    return st_le+le_ex;
+    
+    
+    return answer;
 }
