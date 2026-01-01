a8acd995204d72ddeeae0972426ecb5c706b5067
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-01-01 18:31:22 +0900

    [level 2] Title: 호텔 대실, Time: 0.33 ms, Memory: 3.96 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.cpp
@@ -1,39 +1,32 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <iostream>
-#include <queue>
 
 using namespace std;
 
-int timeToMinutes(string t) {
-    int hour = stoi(t.substr(0, 2));
-    int minute = stoi(t.substr(3, 2));
-    return hour * 60 + minute;
+int convertTime(string time) {
+    return stoi(time.substr(0,2))*60 + stoi(time.substr(3,2));
 }
 
 int solution(vector<vector<string>> book_time) {
     int answer = 0;
-    vector<pair<int, int>> times;
-    for (auto &bt : book_time) {
-        int start = timeToMinutes(bt[0]);
-        int end = timeToMinutes(bt[1]) + 10;
-        times.push_back({start, end});
+    vector<pair<int, int>> v;
+    for(int i=0; i<book_time.size(); i++) {
+        v.push_back({convertTime(book_time[i][0]), convertTime(book_time[i][1])+10});
     }
-
-    sort(times.begin(), times.end());
+    sort(v.begin(), v.end());
     priority_queue<int, vector<int>, greater<int>> pq;
-
-    for (auto &t : times) {
-        int start = t.first, end = t.second;
-
-        if (!pq.empty() && pq.top() <= start) {
-            pq.pop(); 
+    for(int i=0; i<v.size(); i++) {
+        while (!pq.empty() && pq.top() <= v[i].first) {
+            pq.pop();
+            break;
         }
-
-        pq.push(end); 
+        pq.push(v[i].second);
     }
-
-    return pq.size();
+    
+    answer = pq.size();
+    
     return answer;
 }
