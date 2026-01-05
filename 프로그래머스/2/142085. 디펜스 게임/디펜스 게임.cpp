2777f4ab63693ddc64b58409fc90b6e0b1b659a1
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-01-05 23:16:41 +0900

    [level 2] Title: 디펜스 게임, Time: 49.91 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -1,25 +1,21 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int solution(int n, int k, vector<int> enemy) {
-    priority_queue<int> pq;  // 지금까지 나온 적 저장 (큰 순서로)
-    
-    for (int i = 0; i < enemy.size(); i++) {
-        n -= enemy[i];        // 병사 소모
-        pq.push(enemy[i]);    // 이번 라운드 적 기록
-        
-        // 병사가 부족하면 무적권 사용
-        if (n < 0) {
-            if (k > 0) {
-                n += pq.top(); // 가장 큰 적 무적권 처리
-                pq.pop();
-                k--;
-            } else {
-                return i; // 더 이상 진행 불가
-            }
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int sum = 0;
+    for(int i=0; i<enemy.size(); i++) {
+        pq.push(enemy[i]);
+        if (pq.size() > k) {
+            sum += pq.top();
+            pq.pop();
         }
+        if (sum > n) return i;
     }
-    return enemy.size(); // 끝까지 진행 가능
+
+    return enemy.size();
 }
