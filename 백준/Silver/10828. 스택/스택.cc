b19184d0b82b907945fa300e8068b510e07cfbeb
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-03-18 20:35:30 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,41 +1,42 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#include <string>
+
 using namespace std;
-int n, a;
-string s;
-int main() {
-  ios_base::sync_with_stdio(false);
+
+typedef long long ll;
+ll n, s2;
+string s1;
+int main()
+{
+  ios::sync_with_stdio(false);
   cin.tie(NULL);
+  stack<ll> st;
   cin >> n;
-  stack<int> st;
   for (int i = 0; i < n; i++) {
-    cin >> s;
-    if (s == "push") {
-      cin >> a;
-      st.push(a);
-    }
-    else if (s=="pop") {
+    cin >> s1;
+    if (s1 == "push") {
+      cin >> s2;
+      st.push(s2);
+    } else if (s1 == "top") {
       if (st.empty())
-        cout << "-1\n";
+        cout << -1 << "\n";
+      else
+        cout << st.top() << "\n";
+    } else if (s1 == "pop") {
+      if (st.empty())
+        cout << -1 << "\n";
       else {
         cout << st.top() << "\n";
         st.pop();
-      }
-    }
-    else if (s=="size") {
+      }  
+    } else if (s1 == "size") {
       cout << st.size() << "\n";
-    }
-    else if (s=="empty") {
+    } else if (s1 == "empty") {
       if (st.empty())
         cout << "1\n";
       else
         cout << "0\n";
     }
-    else {
-      if (st.empty())
-        cout << "-1\n";
-      else
-        cout << st.top() << "\n";
-    } 
   }
-  return 0;
 }
