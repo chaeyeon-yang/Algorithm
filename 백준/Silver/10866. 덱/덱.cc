a5d3258e1f98f6a26fc975503ef005b9abb82882
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-03-19 20:26:18 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -1,60 +1,61 @@
-#include<bits/stdc++.h>
+#include <queue>
+#include <iostream>
+
 using namespace std;
-int n,a;
+
 string s;
-int main() {
+typedef long long ll;
+ll t, n;
+int main()
+{
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin >> n;
-  deque<int> dq;
-  for (int i = 0; i < n; i++) {
+  cin.tie(0);
+  cin >> t;
+  deque<ll> dq;
+  for (ll i = 0; i < t; i++)
+  {
     cin >> s;
-    if (s=="push_back") {
-      cin >> a;
-      dq.push_back(a);
+    if (s == "push_back") {
+      cin >> n;
+      dq.push_back(n);
     }
-    else if (s=="push_front") {
-      cin >> a;
-      dq.push_front(a);
-    }
-    else if (s=="pop_front"){
+    else if (s == "push_front") {
+      cin >> n;
+      dq.push_front(n);
+    } else if (s == "pop_front") {
       if (dq.empty())
         cout << "-1\n";
       else {
-        int tmp = dq.front();
-        cout << tmp << "\n";
+        cout << dq.front() << "\n";
         dq.pop_front();
       }
-    }
-    else if (s=="pop_back"){
+    } else if (s == "pop_back") {
       if (dq.empty())
         cout << "-1\n";
       else {
-        int tmp = dq.back();
-        cout << tmp << "\n";
+        cout << dq.back() << "\n";
         dq.pop_back();
       }
-    }
-    else if (s=="size") {
+    } else if (s == "size") {
       cout << dq.size() << "\n";
-    }
-    else if (s=="empty") {
+    } else if (s == "empty") {
       if (dq.empty())
         cout << "1\n";
       else
         cout << "0\n";
-    }
-    else if (s=="front") {
-      if (dq.empty()) {
+    } else if (s == "front") {
+      if (dq.empty())
         cout << "-1\n";
-      } else
-        cout << dq.front() << "\n";
-    }
-    else {
-      if (dq.empty()) {
+      else {
+        cout << dq.front()<< "\n";
+      }
+    } else if (s == "back") {
+      if (dq.empty())
         cout << "-1\n";
-      } else
+      else {
         cout << dq.back() << "\n";
+      }
     }
   }
+    return 0;
 }
