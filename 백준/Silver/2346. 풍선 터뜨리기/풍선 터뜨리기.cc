666509f84eaedd8a5c4ad507864ef00b371ba8b0
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-03-19 21:52:17 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -1,37 +1,43 @@
-#include<bits/stdc++.h>
-using namespace std;
-int n;
-deque<pair<int, int>> dq;
-int main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cin >> n;
-  int num;
-  for (int i = 0; i < n; i++) {
-    cin >> num;
-    dq.push_back(make_pair(num, i + 1)); 
-    // 이동해야할 수, 몇 번째
-  }
-  while(!dq.empty()) {
-    int cur = dq.front().first;
-    cout << dq.front().second << " ";
-    dq.pop_front();
-    if (dq.empty())
-      break;
-    if (cur > 0) {
-      for (int i = 0; i < cur-1; i++) {
-        dq.push_back(dq.front());
-        dq.pop_front();
-      }
-    }
-    else {
-      for (int i = 0; i < (-1)*cur; i++) {
-        dq.push_front(dq.back());
-        dq.pop_back();
-      }
-        }
-  }
+#include <iostream>
+#include <deque>
 
-  return 0;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    deque<pair<int,int>> dq;
+
+    for(int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        dq.push_back({i, x});
+    }
+
+    while(!dq.empty()) {
+        auto [idx, move] = dq.front();
+        dq.pop_front();
+
+        cout << idx << " ";
+
+        if(dq.empty()) break;
+
+        if(move > 0) {
+            for(int i = 0; i < move - 1; i++) {
+                dq.push_back(dq.front());
+                dq.pop_front();
+            }
+        } else {
+            for(int i = 0; i < -move; i++) {
+                dq.push_front(dq.back());
+                dq.pop_back();
+            }
+        }
+    }
+
+    return 0;
 }
