9f737552dbaf7fb8307625f8a75d43e256ce5264
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-29 18:37:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
     string tmp = to_string(n);
-    for(int i=0; i<tmp.size(); i++) {
-        answer += tmp[i]-'0';
+    for(char c: tmp) {
+        answer += c-'0';
     }
 
     return answer;
