d6146290de1be263f46078e14e000144eeae47a4
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-30 19:36:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.cpp
@@ -1,15 +1,19 @@
 #include <string>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
 bool solution(int x) {
     bool answer = true;
-    string now = to_string(x);
-    int k = 0;
-    for(char c: now) {
-        k += c-'0';
+    string tmp = to_string(x);
+    vector<int> v(tmp.size());
+    
+    for(char c: tmp) {
+        v.push_back(c-'0');
     }
-    if (x%k != 0) answer = false;
+    int sum = accumulate(v.begin(), v.end(), 0);
+    if (x%sum != 0) answer = false;
+    
     return answer;
 }
