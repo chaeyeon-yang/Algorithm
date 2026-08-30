77edebf45da900eaafe446111ba3219a70843915
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-30 19:16:51 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -8,6 +8,6 @@ long long solution(long long n) {
     long long answer = 0;
     string tmp = to_string(n);
     sort(tmp.rbegin(), tmp.rend());
-    answer = stoll(tmp);
+    answer = stol(tmp);
     return answer;
 }
