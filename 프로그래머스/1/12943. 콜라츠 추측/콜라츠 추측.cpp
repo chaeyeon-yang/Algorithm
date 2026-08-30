4834d99539018c79277feb23fc9c3e24087f2c2e
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-30 21:12:59 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -1,28 +1,21 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 int solution(int num) {
-    typedef long long ll;
-    ll n = num;
     int answer = 0;
-    if (n == 0) return 0;
-    while(n!=1) {
-        answer++;
-        
-        if (answer >= 500) {
-            answer = -1;
-            break;
-        };
-        if (n%2 == 0) {
+    long long n = num;
+    if (n == 1) return 0;
+    while(n!=1 && answer<500) {
+        if (n % 2 == 0) {
             n /= 2;
         } else {
-            n *= 3;
-            n += 1;
+            n = n*3 + 1;
         }
-        
+        answer++;
     }
+    if (answer == 500) answer = -1;
+    
     return answer;
 }
