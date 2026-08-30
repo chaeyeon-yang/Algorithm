c543a52874e5a623a6183fd393fc5728059932f2
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-30 19:21:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 1.82 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -1,15 +1,15 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cmath>
 
 using namespace std;
 
 long long solution(long long n) {
     long long answer = -1;
-    double tmp = sqrt(n);
-    if ((int)tmp == tmp) {
-        answer = (sqrt(n)+1)*(sqrt(n)+1);
+    for(long long i=1; i*i<=n; i++) {
+        if (i*i == n) {
+            answer = (i+1)*(i+1);
+            break;
+        }
     }
     return answer;
 }
