a868c61edb80ab39ecb5c0ee338314f2950681ec
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-08-31 16:29:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.79 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -1,20 +1,30 @@
+#include <string>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-int gcd(int a, int b) {
-    if(a % b == 0) return b;
-    else return gcd(b, a % b);
-}
-
-int lcm(int a, int b) {
-    return a * b / gcd(a, b);
-}
-
 vector<int> solution(int n, int m) {
     vector<int> answer;
+    int min_tmp = min(n, m);
+    int max_tmp = max(n, m);
     
-    answer.push_back(gcd(n, m));
-    answer.push_back(lcm(n, m));
+    int max_v = 0;
+    int min_v = 0;
+    // 최대공약수
+    for(int i=1; i<= min_tmp; i++) {
+        if (n%i == 0 && m%i ==0) {
+            max_v = max(i, max_v);
+        }
+    }
+    // 최소공배수
+    for(int i=min_tmp; i<=m*m; i++) {
+        if (i%n == 0 & i%m ==0) {
+            min_v = i;
+            break;
+        }
+    }
+    answer.push_back(max_v);
+    answer.push_back(min_v);
     return answer;
 }
