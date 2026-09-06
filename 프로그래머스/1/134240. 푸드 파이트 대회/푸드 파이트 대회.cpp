1ef768e300a061624a4b75b74811fb50237e0742
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-06 18:05:58 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.09 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -1,26 +1,34 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 string solution(vector<int> food) {
     string answer = "";
-    string tmp = "";
+    string answer_tmp = "";
+    // 대회에 사용할 수 있는 음식은 짝수 개. 남는 음식은 버려야함
+    map<int, int> mp;
     for(int i=1; i<food.size(); i++) {
-        if (food[i]%2==0) {
-            for(int j=0; j<food[i]/2; j++) {
-                tmp += to_string(i);
-            }
-        } else {
-            for(int j=0; j<food[i]/2; j++) {
-                tmp += to_string(i);
-            }
+        int tmp = food[i]/2;
+        mp[i] = tmp;
+    }
+    for(auto a: mp) {
+        int cal = a.first;
+        int quan = a.second;
+        // cout << cal << " " << quan << "\n";
+        // char to int --> to_string
+        for(int j=0; j<quan; j++) {
+            cout << to_string(cal);
+            answer += to_string(cal);
         }
     }
-    answer += tmp;
+    answer_tmp = answer;
     answer += "0";
-    sort(tmp.rbegin(), tmp.rend());
-    answer += tmp;
+    reverse(answer_tmp.begin(), answer_tmp.end());
+    answer += answer_tmp;
+    
     return answer;
 }
