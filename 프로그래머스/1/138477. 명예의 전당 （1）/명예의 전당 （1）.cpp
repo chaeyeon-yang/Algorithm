5e5f99a758e5956bb5e57fa3cb191930986fabd2
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-08 08:29:07 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.36 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -1,26 +1,23 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
     vector<int> answer;
+    // 명예의 전당 k등
     vector<int> tmp;
     for(int i=0; i<score.size(); i++) {
-        if (tmp.size() < k) {
+        if (tmp.size()<k) {
             tmp.push_back(score[i]);
-        } 
-        else {
             sort(tmp.begin(), tmp.end());
-            if (score[i] > tmp[0]) {
-                tmp.erase(tmp.begin());
-                tmp.push_back(score[i]);
-            }
+        } else if (score[i] > tmp[0]) {
+            tmp.erase(tmp.begin());     
+            tmp.push_back(score[i]);
+            sort(tmp.begin(), tmp.end());
         }
-        sort(tmp.begin(), tmp.end());
-        answer.push_back(tmp[0]);
+        answer.push_back(tmp[0]); 
     }
     return answer;
 }
