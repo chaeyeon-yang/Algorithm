4607a868bc2fa95c829d437b738a3d83dd0e8147
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-07 21:21:08 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int solution(int a, int b, int n) {
     int answer = 0;
-    int rest = n;
-    while(rest/a!=0) {
-        int tmp = rest/a;
-        rest = rest%a + b*tmp;
-        
-        answer += b*tmp;
+    // 빈 병 a -> 콜라 b
+    // 빈 병 n -> ?
+    
+    // 빈 병 5개 주면 콜라 3개
+    while (n >= a) {
+        int got = (n/a)*b;    
+        answer += got;           
+        n = (n % a) + got; // 남은 빈병 + 새로 받은 콜라
     }
+    
     return answer;
 }
