5cf0cae27d6eade09e664de42b584d93feac0ca7
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-09 15:17:12 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -1,24 +1,22 @@
 #include <string>
 #include <vector>
-#include <iostream>
+#include <map>
 
 using namespace std;
 
-// 윤년!!
-// 31 29 31 30 31 30 31 30 31 30 31 30
-// 금 토 일 월 화 수 목 
 string solution(int a, int b) {
     string answer = "";
-    vector<string> wee = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
-    vector<int> days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    int s = 0;
-
-    for(int i=0; i<a-1; i++) {
-        s += days[i];
-    }
-    s += b;
-    answer = wee[(s+6)%7];
-   
+    int arr[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    map<int, string> mp = {{0, "THU"},{1, "FRI"}, {2, "SAT"}, {3, "SUN"}, {4, "MON"}, {5, "TUE"}, {6, "WED"}, {7, "THU"}};
+    // 윤년은 2월이 29일까지 
+    // 금 토 일 월 화 수 목
+    
+    // 31 29 31 30
+    int cnt = 0;
+    for(int i=0; i<a; i++) {
+        cnt += arr[i];
+    } 
+    cnt += b;
+    answer = mp[cnt%7];
     return answer;
 }
--- 프로그래머스/1/12901. 2016년/README.md
+++ 프로그래머스/1/12901. 2016년/README.md
@@ -1,10 +1,10 @@
 # [level 1] 2016년 - 12901 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12901) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.01 ms
+메모리: 5.07 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 30일 14:38:56
+2026년 09월 09일 15:17:10
 
 ### 문제 설명
 
