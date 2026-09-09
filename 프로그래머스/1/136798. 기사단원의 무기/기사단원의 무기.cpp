8fb6d19e1ac53943e3f70207709abadc0900be58
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-09 14:52:05 +0900

    [level 1] Title: 기사단원의 무기, Time: 7784.02 ms, Memory: 5.08 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -1,15 +1,14 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
-int measure(int num) {
+// 약수 개수 구하기
+int yaksu(int num) {
     int cnt = 0;
-    for(int i=1; i*i<=num; i++) {
+    for(int i=1; i<=num; i++) {
         if (num%i == 0) {
             cnt++;
-            if (i != num/i) cnt++;
         }
     }
     return cnt;
@@ -17,10 +16,18 @@ int measure(int num) {
 
 int solution(int number, int limit, int power) {
     int answer = 0;
+    // 1 2 3 4 5
+    // 약수 개수 1 1 1 1 1
+    // 제한 3
+    // 약수 개수가 제한보다 크면 POWER
     for(int i=1; i<=number; i++) {
-        int now = measure(i);
-        if (now > limit) answer += power;
-        else answer += now;
+        int cnt = yaksu(i);
+        if (cnt > limit) {
+            answer += power;
+        } else {
+            answer += cnt;
+        }
     }
+    
     return answer;
 }
