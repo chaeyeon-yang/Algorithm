a255fbd29e21a6dc4381f60f068f94021f32a530
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-17 22:17:19 +0900

    [level 1] Title: 과일 장수, Time: 19.32 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -6,11 +6,16 @@ using namespace std;
 
 int solution(int k, int m, vector<int> score) {
     int answer = 0;
-    sort(score.rbegin(), score.rend());
     
-    for(int i = 0; i + m <= score.size(); i += m) {
-        int min_score = score[i + m - 1];
-        answer += min_score * m;
+    // 사과 1점 ~ k점
+    // 한 상자에 사과 m개씩 담아 포장
+    // 상자에 담긴 사과 중 가장 낮은 점수가 p 점인 경우 가격은 p*m
+    // 가능한 많은 사과를 팔았을 때 얻을 수 있는 최대 이익
+    
+    sort(score.rbegin(), score.rend());
+    for(int i=0; i<score.size(); i+=m) {
+        if (score.size()-i < m) break;
+        answer += score[i+m-1]*m;
     }
     
     return answer;
