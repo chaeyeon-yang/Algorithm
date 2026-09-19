384314eb9d4ff3f5d4f4b10b0155e0e6c277125b
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-19 16:41:58 +0900

    [level 1] Title: 둘만의 암호, Time: 0.17 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.02 ms
+메모리: 4.79 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 21일 22:37:42
+2026년 09월 19일 16:41:55
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -6,18 +6,27 @@ using namespace std;
 
 string solution(string s, string skip, int index) {
     string answer = "";
-    for (char c : s) {
+    for(char c: s) {
         int cnt = 0;
-        char next = c;
-        
+        int now = stoi(to_string(c));
+        cout << "now : " << now << " \n";
         while (cnt < index) {
-            next++;
-            if (next > 'z') next = 'a';  
-
-            if (skip.find(next) != string::npos) continue; 
-            cnt++;
+            now += 1;
+            now = 97+(now-97)%26;
+            bool flag = false;
+            for(char sk: skip) {
+                if (stoi(to_string(sk)) == now) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag) {
+                cnt++;
+            }
         }
-        answer += next;
+        answer += static_cast<char>(now);
     }
+    
+    
     return answer;
 }
