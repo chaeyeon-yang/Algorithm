799db99bfc7e6807f37b7ab22574996acb218850
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-19 16:49:45 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -5,22 +5,16 @@ using namespace std;
 
 int solution(vector<vector<string>> board, int h, int w) {
     int answer = 0;
-    string b[8][8];
-    const int dy[4] = {-1,0,1,0};
-    const int dx[4] = {0,1,0,-1};
-    
-    for(int i=0; i<board.size(); i++) {
-        for(int j=0; j<board[i].size(); j++) {
-            b[i][j] = board[i][j];
-        }
-    }
-    string color = b[h][w];
+    int n = board.size(); 
+    const int dh[4] = {0, 1, -1, 0};
+    const int dx[4] = {1, 0, 0, -1};
     
     for(int i=0; i<4; i++) {
-        int ny = h+dy[i];
-        int nx = w+dx[i];
-        if (ny<0 || ny > 7 || nx<0 || nx>7) continue;
-        if (b[ny][nx] == color) answer++;
+        int h_check = h + dh[i];
+        int w_check = w + dx[i];
+        if (h_check <0 || h_check >=n || w_check <0 || 
+            w_check > n) continue;
+        if (board[h][w] == board[h_check][w_check]) answer++;
     }
     
     return answer;
