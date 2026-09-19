5824f457d730893a57763e5d8da1c19ed8dc00c0
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-19 17:12:58 +0900

    [level 1] Title: 문자열 나누기, Time: 0.22 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -1,34 +1,38 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <map>
+#include <iostream>
 
 using namespace std;
 
 int solution(string s) {
     int answer = 0;
-    map<char, int> mp;
-    int tmp = -1;
-    char c = s[0];
-    for(int i=0; i<s.size(); i++) {
-        // cout << i << "번째 인덱스 실행 ~~ \n";
-        if (s[i] == c) {
-            mp[c]++;
-            tmp = mp[c];
-        } else {
-            mp['A']++;
+    
+    vector<string> v;
+    string tmp = string(1, s[0]);
+    
+    char x = s[0];
+    
+    int x_cnt = 1;
+    int other_cnt = 0;
+    
+    for (int i=1; i<s.size(); i++) {
+        if (x_cnt == 0 && other_cnt ==0) {
+            x = s[i];
         }
-        
-        // cout << "s[i] : " << s[i] << " tmp : " << tmp << " c : " << c <<  "\n";
-        
-        if (s[i] != c && mp['A'] == tmp) {
-            answer++;
-            // cout << "-----" << c << s[i] << "-----";
-            mp[c] = mp['A'] = 0;
-            c = s[i+1];
-            // cout << i+1 << " 번째로 이동 " << c << "가 이젠 첫문자!\n";
+        if (s[i] == x) {
+            x_cnt++;
+        } else if (s[i]!=x) {
+            other_cnt++;
+        }
+        tmp += s[i];
+        if (x_cnt == other_cnt) {
+            x_cnt = other_cnt = 0;
+            v.push_back(tmp);
+            tmp = "";
         }
     }
-    if (mp[c] > 0 || mp['A'] >0) answer++;
+    answer = v.size();
+    if (tmp!="") answer++;
     return answer;
 }
