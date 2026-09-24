253de59f169cd6552509e12a9d4f4aa90e7dc896
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-24 16:03:26 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 4.77 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -5,15 +5,27 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    
+    int sum = 0;
+
     for(int i=1; i<=n; i++) {
-        int sum = 0;
-        int idx = i;
-        while(sum < n) {
-            sum += idx++;
+        int sum = i;
+        if (sum == n) {
+            answer++;
+            continue;
         }
-        if (sum == n) answer++;
+        for(int j=i+1; j<=n; j++) {
+            sum += j;
+            if (sum == n) {
+                answer++;
+                sum = 0;
+                break;
+            }
+            if (sum > n) {
+                break;
+            }
+        }
+        
     }
-    // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+    
     return answer;
 }
