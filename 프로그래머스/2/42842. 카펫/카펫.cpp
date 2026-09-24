aec4ca51e6eeef756087b322a05ecc9d6e73238a
양채연스 <69382168+chaeyeon-yang@users.noreply.github.com>
2026-09-24 18:07:44 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,28 +1,22 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    // yellow 2, brown 10
-    // -> y-가로x2, y-세로x2 + 4(모서리) = brown
-    // y-가로+2, y-세로+2 => 전체 카펫의 가로, 세로
-    // y-가로 x y-세로 = y
-    // 가로 >= 세로
-    // 2x+2y = 20, x+y=10 // (9,1) (8,2) (7,3) (6,4) (5,5) 
+    long long min_v = min(brown, yellow);
     
-    int tmp = (brown-4)/2;
-    vector<pair<int, int>> now;
-    
-    for(int i=1; i<brown; i++) {
-        if (i>tmp-i) break;
-        if ((tmp-i)*i == yellow)
-            now.push_back({tmp-i, i});
+    for(long long i=1; i<=min_v; i++) {
+        if (i+(yellow/(double)i) == (double)(brown-4)/2) {
+            answer.push_back(i+2);
+            answer.push_back(yellow/i+2);
+            break;
+        }
     }
+    sort(answer.rbegin(), answer.rend());
     
-    answer.push_back(now[0].first+2);
-    answer.push_back(now[0].second+2);
-
     return answer;
 }
